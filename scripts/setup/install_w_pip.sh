a0e85cc710ff324f27b7194fa6e63dfc9688eead
Jonas F Henriksen <jonas.henriksen@uib.no>
2018-04-30 11:19:38 +0200

    (#2) Improved install script (no actually works)

--- scripts/setup/install_w_pip.sh
+++ scripts/setup/install_w_pip.sh
@@ -4,9 +4,49 @@
 #
 # Install Django and other packages with pip
 #
-# To save the current setup in the requirements file, run: 
+# make sure you have installed python and pip
+# (These are platform dependent)
+# apt install python
+# Pip:
+# apt install python-pip
+# Ubuntu also needs:
+# sudo apt-get install libmysqlclient-dev
+# Upgrade Pip:
+# pip install --upgrade pip
+# Virtualenv:
+# pip install virtualenv
+#
+# To save the current setup in the requirements file, run:
 # pip freeze >requirements.txt
 #
 ################################################################################
-git clone https://github.com/django/django.git
-pip install -r requirements.txt
+if [ -z "$(which pip)" ]
+then
+  echo "pip is not installed. Please install pip to continue."
+  exit
+fi
+pip install --upgrade pip
+if [ -z "$(which virtualenv)" ]
+then
+  pip install virtualenv
+fi
+
+if [ ! -d .env ]
+then
+  virtualenv -p python3 --no-site-packages --distribute .env
+fi
+
+if [[ -z "$VIRTUAL_ENV" ]]
+then
+    source .env/bin/activate
+fi
+if [ ! -d django ]
+then
+  git clone https://github.com/django/django.git
+fi
+
+pip install -r scripts/setup/requirements.txt
+
+echo "Remember to set up the database if you have not already"
+echo "Instructions: "
+cat scripts/setup/mariadb_setup.md
