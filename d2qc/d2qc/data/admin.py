fdd0e5e340ea7c15dd9988b17a7a89640eb98602
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2018-05-05 11:42:12 +0200

    (#12) Enable admin interface for models

--- d2qc/d2qc/data/admin.py
+++ d2qc/d2qc/data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from d2qc.data.models import *
 
-# Register your models here.
+admin.site.register(DataSet)
+admin.site.register(DataType)
+admin.site.register(DataPoint)
+admin.site.register(DataValue)
