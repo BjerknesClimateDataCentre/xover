aa831b474046a3c344b7cf0ec7b57f0ba838ba92
Jonas F Henriksen <jonas.henriksen@uib.no>
2018-12-04 15:56:23 +0100

    (#37) Views for handling data files
    - Supports create, update, delete, list, details
    - Very simple template layout
    - Urls for the different views
    - Simple navigation menu

---
+++ d2qc/d2qc/data/context_processors.py
@@ -0,0 +1,9 @@
+
+def globals(request):
+    """Constants available to every template page"""
+
+    global_vars = {
+        # Site name
+        'SITE_HEADER': "GLODAP Crossover QC tools"
+    }
+    return global_vars
