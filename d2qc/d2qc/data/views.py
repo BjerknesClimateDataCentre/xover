28719f91d2aaa99880e7cc26e217aa14af14adb2
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-01-28 16:13:48 +0100

    (#39) Fetch matching crossover stations

--- d2qc/d2qc/data/views.py
+++ d2qc/d2qc/data/views.py
@@ -462,6 +462,9 @@ class DataSetDetail(DetailView):
         context['stations'] = self.get_object().get_stations(
             self.kwargs.get('parameter_id')
         )
+        context['crossovers'] = self.get_object().get_crossover_stations(
+            self.kwargs.get('parameter_id')
+        )
         return context
 
 class DataSetDelete(DeleteView):
