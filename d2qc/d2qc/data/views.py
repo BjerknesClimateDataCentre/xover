62f75e7bbedc35c16f274be80881b9c30d8f1e96
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-01-29 14:48:56 +0100

    (#60) Create and manage user settings

--- d2qc/d2qc/data/views.py
+++ d2qc/d2qc/data/views.py
@@ -470,3 +470,24 @@ class DataSetDelete(DeleteView):
             )
         )
         return super().delete(request, *args, **kwargs)
+
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['min_depth','crossover_radius']
+    def get_success_url(self):
+        return reverse('data')
+    def get_object(self):
+        try:
+            return self.request.user.profile
+        except:
+            self.request.user.profile = Profile(user=self.request.user)
+            self.request.user.save()
+        return self.request.user.profile
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.success(
+            self.request,
+            "Profile settings updated"
+        )
+        return super().form_valid(form)
