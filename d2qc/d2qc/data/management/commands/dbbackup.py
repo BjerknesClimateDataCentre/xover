1b063ae2c70a7cca757f9b26e018ed1493f3ef70
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-03-05 09:26:30 +0100

    (#84) Update backup script to work with postgres

--- d2qc/d2qc/data/management/commands/dbbackup.py
+++ d2qc/d2qc/data/management/commands/dbbackup.py
@@ -8,23 +8,38 @@ import os
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        # Make a backup of the database
+        '''Make a backup of the database
+        '''
 
         db_name = settings.DATABASES['default']['NAME']
         user = settings.DATABASES['default']['USER']
         pw = settings.DATABASES['default']['PASSWORD']
         host = settings.DATABASES['default']['HOST']
         port = settings.DATABASES['default']['PORT']
-        file = settings.BACKUP_FOLDER
-        if not os.path.isdir(file):
-            os.makedirs(file)
+        _file = settings.BACKUP_FOLDER
+        if not os.path.isdir(_file):
+            os.makedirs(_file)
+        pgpass_file = os.path.expanduser('~/.pgpass')
+        if not os.path.isfile(pgpass_file):
+            with open(pgpass_file, 'a') as pgpass:
+                pgpass.write("{}:{}:{}:{}:{}".format(
+                        host, port or '*', user, pw, db_name
+                    )
+                )
+            os.chmod(pgpass_file, 0o600)
 
-        file += '/d2qc-dbbackup-'
-        file += datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        file += '.sql'
-        cmd = "mysqldump --default-character-set=UTF8 "
-        cmd += "--user={} --password={} ".format(user, pw)
-        cmd += "--host={} --port={} ".format(host, port)
-        cmd += "{} ".format(db_name)
-        cmd += " | gzip - > '{}.gz' ".format(file)
+        _file += '/d2qc-dbbackup-'
+        _file += datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        _file += '.dump'
+        cmd = "pg_dump "
+        cmd += "--file {} ".format(_file)
+        cmd += "--username {} ".format(user)
+        cmd += "--host {} ".format(host)
+        if port:
+            cmd += "--port {} ".format(port)
+        cmd += "--compress 9 --format c "
+        cmd += "d2qc"
         os.system(cmd)
+        latest = settings.BACKUP_FOLDER + "/latest.dump"
+        os.remove(latest)
+        os.symlink(_file, latest)
