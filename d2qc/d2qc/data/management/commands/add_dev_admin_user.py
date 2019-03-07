0b1e92d79aa469a6873e19e9983c3eec03d55226
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-03-07 13:19:40 +0100

    (#84) Ability to configure dev admin user

---
+++ d2qc/d2qc/data/management/commands/add_dev_admin_user.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model;
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+class Command(BaseCommand):
+    '''### Only use on development environments ###
+    Create a admin user with username admin, password 123. If this user
+    already exists, just set the password to 123, so you can access the admin
+    account easily.
+    '''
+
+    def handle(self, *args, **options):
+        '''Restore the database
+        '''
+        if not hasattr(settings, 'DEV_ADMIN_PROPERTIES'):
+            raise CommandError(
+                "DEV_ADMIN_PROPERTIES not set in settings. See "
+                + "/d2qc/d2qc/setup/sample.development.py for an example."
+            )
+
+        User = get_user_model()
+        admin,b=User.objects.get_or_create(
+            username=settings.DEV_ADMIN_PROPERTIES['user']
+        )
+        admin.is_superuser = True
+        admin.is_staff = True
+        admin.set_password(settings.DEV_ADMIN_PROPERTIES['password'])
+        admin.email = settings.DEV_ADMIN_PROPERTIES['email']
+        admin.save()
--- scripts/setup/vagrant_dev_provisioning.sh
+++ scripts/setup/vagrant_dev_provisioning.sh
@@ -9,13 +9,4 @@
 cd /vagrant
 source .env_vagrant/bin/activate
 cd d2qc
-echo "
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-admin,b=User.objects.get_or_create(username='admin')
-admin.is_superuser = True
-admin.is_staff = True
-admin.set_password('123')
-admin.email = 'jhe052@uib.no'
-admin.save()
-" | python manage.py shell
+python manage.py add_dev_admin_user
