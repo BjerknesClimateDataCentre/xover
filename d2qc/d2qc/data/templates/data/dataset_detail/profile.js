6a954426da8a7618dd860bc746e86053093e285d
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-08-21 14:05:21 +0200

    (#115) Organize dataset_detail template

---
+++ d2qc/d2qc/data/templates/data/dataset_detail/profile.js
@@ -0,0 +1,172 @@
+// Profiles:
+var data = [];
+var profiles = [];
+var interp_profiles = [];
+var profiles_ref = [];
+var interp_profiles_ref = [];
+var stats = false;
+
+// Profiles dataset
+{% if dataset_profiles %}
+  var profiles = JSON.parse('{{ dataset_profiles | escapejs }}');
+{% endif %}
+{% if dataset_interp_profiles %}
+  var interp_profiles = JSON.parse('{{ dataset_interp_profiles | escapejs }}');
+{% endif %}
+
+// Reference dataset
+{% if dataset_ref_profiles %}
+  var profiles_ref = JSON.parse('{{ dataset_ref_profiles | escapejs }}');
+{% endif %}
+{% if dataset_ref_interp_profiles %}
+  var interp_profiles_ref = JSON.parse('{{ dataset_ref_interp_profiles | escapejs }}');
+{% endif %}
+
+// Stats
+{% if dataset_stats %}
+  stats = JSON.parse('{{ dataset_stats | escapejs }}');
+{% endif %}
+
+var current_dataset = 0;
+var data_set_id = parseInt('{{ object.id }}')
+
+
+for (var i=0; i<interp_profiles.length; i++) {
+    var showlegend = false
+    if (i == 0) {
+        showlegend = true
+    }
+    profile = createProfile(
+        interp_profiles[i],
+        blue_profile,
+        showlegend,
+        true
+    )
+    if(profile) {
+        data.push(profile)
+    }
+}
+
+// Min and max values for the plotting parameter
+var param_max = Number.MIN_VALUE
+var param_min = Number.MAX_VALUE
+
+for (var i=0; i<profiles.length; i++) {
+    profile = createProfile(profiles[i], blue_profile, false, false)
+    if(profile) {
+        data.push(profile)
+        param_max = Math.max(param_max, ...profiles[i].param.data)
+        param_min = Math.min(param_min, ...profiles[i].param.data)
+    }
+}
+for (var i=0; i<profiles_ref.length; i++) {
+    profile = createProfile(profiles_ref[i], red_profile, false, false)
+    if(profile) {
+        data.push(profile)
+        param_max = Math.max(param_max, ...profiles_ref[i].param.data)
+        param_min = Math.min(param_min, ...profiles_ref[i].param.data)
+    }
+}
+for (var i=0; i<interp_profiles_ref.length; i++) {
+    var showlegend = false
+    if (i == 0) {
+        showlegend = true
+    }
+    profile = createProfile(interp_profiles_ref[i], red_profile, showlegend, true)
+    if(profile) {
+        data.push(profile)
+    }
+}
+
+// Check stats are not empty
+if (stats) {
+    // Indicate where statistics are valid
+    min_stat = Math.min(...stats.y)
+    max_stat = Math.max(...stats.y)
+    diff_stat = max_stat-min_stat
+    diff_param = param_max - param_min
+    var stats_limits = {
+        x: [
+            param_min,
+            param_max,
+            param_max,
+            param_max + diff_param / 14,
+        ],
+        y: [
+            min_stat,
+            min_stat,
+            min_stat - diff_stat / 5,
+            min_stat + diff_stat / 2,
+        ],
+        mode: 'lines',
+        type: 'scatter',
+        line: {
+          color: bluetransp_profile,
+        },
+        marker: {
+          color: bluetransp_profile,
+        },
+        showlegend: false
+    }
+    stats_fill = JSON.parse(JSON.stringify(stats_limits))
+    stats_fill.y = [
+        max_stat,
+        max_stat,
+        max_stat + diff_stat / 5,
+        max_stat - diff_stat / 2,
+    ]
+    stats_fill.fill = 'tonexty'
+    stats_fill.fillcolor = bluetransp_profile
+    stats_fill.showlegend = true
+    stats_fill.name = 'Matching crossover area'
+
+    // Add these to the beginning of the data array
+    data.unshift(stats_fill)
+    data.unshift(stats_limits)
+}
+
+var layout = {
+    yaxis: {autorange: 'reversed',title: {text:'Sigma 4'}},
+    xaxis: {title: {text:profiles[0].parameter}},
+    margin: {l: 60, r:0},
+    legend: {orientation: 'h'},
+    hovermode: 'y'
+};
+Plotly.newPlot('profiles', data, layout);
+/* create a profile for the profile plot */
+function createProfile(p, color, showlegend, line) {
+    if (!('param' in p)) {
+        return []
+    }
+
+    var profile = {
+        x: Object.values(p.param.data),
+        y: Object.values(p.sigma4.data),
+        mode: 'markers',
+        showlegend: showlegend,
+        name: '',
+        marker: {
+          color: color,
+          size: 3,
+        },
+    }
+    if (line) {
+      profile.mode = 'lines'
+      profile.line = {
+        color: color,
+        width: 1,
+      }
+      profile.hoverinfo = 'skip'
+    }
+    if (showlegend) {
+        profile.name = p.expocode
+    }
+    return profile
+}
+
+{% if dataset_stats %}
+  {% include "./profile/stats.js"%}
+{% endif %}
+{% if summary_stats %}
+  {% include "./profile/summary_stats.js"%}
+{% endif %}
---
+++ d2qc/d2qc/data/templates/data/dataset_detail/profile/summary_stats.js
@@ -0,0 +1,73 @@
+var summary_stats = JSON.parse('{{ summary_stats | escapejs }}');
+w_mean = {
+  x: summary_stats.date,
+  y: summary_stats.w_mean,
+  text: summary_stats.expocode,
+  textposition: 'top',
+  error_y: {
+    type: 'data',
+    array: summary_stats.w_stdev,
+    visible: true
+  },
+  mode: 'markers+text',
+  type: 'scatter',
+  line: {
+    color: 'black'
+  },
+  hoverinfo: 'text',
+  textposition: 'top right',
+  name: 'Weighted mean &plusmn; st. dev'
+};
+mean = {
+  x: summary_stats.date,
+  y: summary_stats.mean,
+  mode: 'lines',
+  type: 'scatter',
+  line: {
+    color: red_profile
+  },
+  name: 'Weighted mean',
+  hoverinfo: 'text',
+  hovertext:
+    'Mean ' + summary_stats.mean[0].toFixed(4) + '<br>'
+      + 'Stdev &plusmn;' + summary_stats.stdev[0].toFixed(4)
+};
+stdev = {
+  x: summary_stats.date,
+  y: summary_stats.mean.map(function(x, i){return x + summary_stats.stdev[i];}),
+  mode: 'lines',
+  type: 'scatter',
+  line: {
+    color: red_profile,
+    dash: 'dot'
+  },
+  name: 'Stdev',
+  hoverinfo: 'skip'
+};
+stdev2 = {
+  x: summary_stats.date,
+  y: summary_stats.mean.map(function(x, i){return x - summary_stats.stdev[i];}),
+  mode: 'lines',
+  type: 'scatter',
+  line: {
+    color: red_profile,
+    dash: 'dot'
+  },
+  showlegend: false,
+  hoverinfo: 'skip'
+};
+
+data = [
+  w_mean,
+  mean,
+  stdev,
+  stdev2
+]
+layout = {
+    yaxis: {title: {text:'Offset'}},
+    margin: {l: 60, r:0},
+    legend: {orientation: 'h'},
+    hovermode: 'x'
+};
+
+Plotly.newPlot('summary-stats', data, layout);
