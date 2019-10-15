b8dcc6e84314542070af17a728f615b9c93d023d
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-10-15 12:36:05 +0200

    (#159) Include depth-metric and only-qc-data - setting in profile

--- d2qc/d2qc/data/models/profile.py
+++ d2qc/d2qc/data/models/profile.py
@@ -2,6 +2,12 @@ from django.contrib.gis.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
+    SIGMA4 = 'sigma4'
+    DEPTH = 'depth'
+    DEPTH_METRICS = [
+        (SIGMA4, 'Sigma 4'),
+        (DEPTH, 'Depth'),
+    ]
     class Meta:
         db_table = 'd2qc_user_profile'
     user = models.OneToOneField(
@@ -14,5 +20,22 @@ class Profile(models.Model):
     )
     min_depth = models.IntegerField(default=1500)
     crossover_radius = models.IntegerField(default=200000)
+    depth_metric = models.CharField(
+        choices = DEPTH_METRICS,
+        max_length = 20,
+        default = SIGMA4,
+    )
+    only_qc_controlled_data = models.BooleanField(
+        default = False,
+    )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        str = {
+            'min_depth': self.min_depth,
+            'crossover_radius': self.crossover_radius,
+            'depth_metric': self.depth_metric,
+            'only_qc_controlled_data': self.only_qc_controlled_data,
+        }
+        return str(str)
