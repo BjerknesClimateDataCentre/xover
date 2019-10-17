690f02d6e6e083609a5d30b3f53be83b5e8bea42
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-10-17 11:28:45 +0200

    (#42) New field sigma4 in depth model

--- d2qc/d2qc/data/models/depth.py
+++ d2qc/d2qc/data/models/depth.py
@@ -13,6 +13,12 @@ class Depth(models.Model):
     cast = models.ForeignKey('Cast',
             related_name='depths', on_delete=models.CASCADE)
     depth = models.DecimalField(max_digits=8, decimal_places=3)
+    sigma4 = models.DecimalField(
+        max_digits = 8,
+        decimal_places = 5,
+        blank = True,
+        null = True,
+    )
     date_and_time = models.DateTimeField()
     bottle = models.IntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
