261ae97ee52f2c3b389049de7cf46d7323982846
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-11-01 11:26:23 +0100

    (#170) Option to update data types

--- d2qc/d2qc/data/management/commands/init_data_types.py
+++ d2qc/d2qc/data/management/commands/init_data_types.py
@@ -9,6 +9,16 @@ class Command(NewlineCommand):
         types. If a data type or a data type name already exists, it is not
         changed.
     """
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-u',
+            '--update',
+            action='store_true',
+            help=(
+                "Update existing data types. May roll back changes made "
+                + "by other means in the database"
+            ),
+        )
 
     def handle(self, *args, **options):
         '''Insert data types'''
@@ -21,40 +31,70 @@ class Command(NewlineCommand):
                 if data_type.name in reference_types:
                     ref_type = reference_types[data_type.name]
                 else:
-                    ref_type, created = DataType.objects.get_or_create(
+
+                    ref_type, new = self._add_datatype(
+                        name = data_type.name,
                         identifier = data_type.identifier,
-                        original_label = data_type.name,
+                        update = options['update'],
                     )
-                    if created:
+                    if new:
                         new_data_types[ref_type.original_label] = ref_type
             elif data_type.parent_ref_type:
                 if data_type.parent_ref_type.name in reference_types:
                     ref_type = reference_types[data_type.parent_ref_type.name]
                 else:
-                    ref_type, created = DataType.objects.get_or_create(
+                    ref_type, new = self._add_datatype(
+                        name = data_type.parent_ref_type.name,
                         identifier = data_type.parent_ref_type.identifier,
-                        original_label = data_type.parent_ref_type.name,
+                        update = options['update'],
                     )
-                    if created:
+                    if new:
                         new_data_types[ref_type.original_label] = ref_type
             else:
                 # No reference type, skip this one
                 continue
             reference_types[ref_type.original_label] = ref_type
-            data_type_name, created = DataTypeName.objects.get_or_create(
+            data_type_name, new = DataTypeName.objects.get_or_create(
                 name = data_type.name,
-                data_type = ref_type,
             )
-            if created:
+            if options['update'] and not new:
+                if data_type_name.data_type_id != ref_type.id:
+                    data_type_name.data_type_id = ref_type.id
+                    data_type_name.save()
+                    if options['verbosity'] > 0:
+                        self.stdout.write(
+                            f"Updated data type for {data_type_name}"
+                        )
+            if new:
+                data_type_name.data_type_id = ref_type.id
+                data_type_name.save()
                 new_data_type_names[data_type_name.name] = data_type_name
         if options['verbosity'] > 0:
-            print("""Data types initialized. """, end='')
             if new_data_types or new_data_type_names:
-                print("""The following was added""")
+                self.stdout.write(
+                    "Data types initialized, the following was added"
+                )
                 if new_data_types:
-                    print("Data Types:")
-                    print(', '.join(map(str, new_data_types)))
+                    self.stdout.write("Data Types:")
+                    self.stdout.write(', '.join(map(str, new_data_types)))
                 if new_data_type_names:
-                    print("Data Type Names:")
-                    print(', '.join(map(str, new_data_type_names)))
-            print("")
+                    self.stdout.write("Data Type Names:")
+                    self.stdout.write(', '.join(map(str, new_data_type_names)))
+            else:
+                self.stdout.write("""No new data types where added""")
+
+    def _add_datatype(self, name, identifier, update):
+        obj, new = DataType.objects.get_or_create(
+            original_label = name,
+        )
+        if update and not new:
+            if obj.identifier != identifier:
+                obj.identifier = identifier
+                obj.save()
+                if options['verbosity'] > 0:
+                    self.stdout.write(f"Updated identifier for {obj}")
+        elif new:
+            obj.identifier = identifier
+            obj.save()
+
+        return (obj, new)
