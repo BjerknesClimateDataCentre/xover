7b7db9aa09ffec0240db4256b344af69e35357bc
Jonas F. Henriksen <jonas.f.henriksen@gmail.com>
2019-11-19 12:51:46 +0100

    (#182) Handle if mean salinity is NaN

--- d2qc/d2qc/data/management/commands/normalize_refdata.py
+++ d2qc/d2qc/data/management/commands/normalize_refdata.py
@@ -10,6 +10,8 @@ class Command(NewlineCommand):
         The function loops over the reference data sets, and normalizes the
         alkalinity and co2 data against salinity. Data sets / parameters that
         are already normalized are excluded.
+
+        This functions generally takes a few minutes to run.
     """
 
     def handle(self, *args, **options):
@@ -18,11 +20,17 @@ class Command(NewlineCommand):
         for data_set in models.DataSet.objects.filter(is_reference=True):
             data_type_names = data_set.getNormalizableParameters(user.profile)
             for data_type_name in data_type_names:
-                data_set.normalize_data(
-                    data_type_name.id,
-                    user
-                )
-            self.stdout.write(f"Normalized data set {data_set}")
+                try:
+                    data_set.normalize_data(
+                        data_type_name.id,
+                        user
+                    )
+                except Exception as e:
+                    self.stderr.write(f"{e}")
+
+
+            if options['verbosity']>1:
+                self.stdout.write(f"Normalized data set {data_set}")
 
 
 
