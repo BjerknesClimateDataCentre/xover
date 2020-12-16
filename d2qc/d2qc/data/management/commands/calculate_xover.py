662e494da0264d54cb898e44a71053c7aac56e6c
Maren <maren.kjos@gmail.com>
2020-12-16 13:26:34 +0100

    (#191) Added try-except clause surrounding summary calculation

--- d2qc/d2qc/data/management/commands/calculate_xover.py
+++ d2qc/d2qc/data/management/commands/calculate_xover.py
@@ -69,6 +69,7 @@ class Command(NewlineCommand):
         only_qc_controlled_data = options['only_qc_controlled_data']
         minimum_num_stations = options['minimum_num_stations']
 
+
         # Return cached value if exists
         cache_key = "calculate_xover-{}-{}-{}-{}-{}-{}-{}".format(
             data_set_id,
@@ -84,106 +85,118 @@ class Command(NewlineCommand):
             return value
 
 
-        data_set = models.DataSet.objects.get(pk=data_set_id)
-        data_set_stations = data_set.get_stations(
-            parameter_id = parameter_id,
-            crossover_radius = crossover_radius,
-            min_depth = min_depth,
-        )
-        data_sets = data_set.get_station_data_sets(
-            data_set.get_crossover_stations(
-                stations = data_set_stations,
+        try:
+            
+            data_set = models.DataSet.objects.get(pk=data_set_id)
+            data_set_stations = data_set.get_stations(
                 parameter_id = parameter_id,
-                min_depth = min_depth,
-                minimum_num_stations = 3,
                 crossover_radius = crossover_radius,
-            )
-        )
-        data = {
-            'w_mean': [],
-            'w_stdev': [],
-            'expocode': [],
-            'data_set_id': [],
-            'date': [],
-        }
-        for ds in data_sets:
-            # Get stations from this data set that falls within the crossover
-            # radius of the original data set
-            crossover_stations = data_set.get_crossover_stations(
-                stations = data_set_stations,
-                parameter_id = parameter_id,
-                crossover_data_set_id = ds[0],
                 min_depth = min_depth,
-                minimum_num_stations = minimum_num_stations,
-                crossover_radius = crossover_radius,
             )
-            # Get stations from the original data set that match this data set
-            crossed_data_set_stations = data_set.get_crossover_stations(
-                stations = crossover_stations,
-                parameter_id = parameter_id,
-                crossover_data_set_id = data_set_id,
-                min_depth = min_depth,
-                crossover_radius = crossover_radius,
+            data_sets = data_set.get_station_data_sets(
+                data_set.get_crossover_stations(
+                    stations = data_set_stations,
+                    parameter_id = parameter_id,
+                    min_depth = min_depth,
+                    minimum_num_stations = 3,
+                    crossover_radius = crossover_radius,
+                )
             )
-            if (
-                    len(crossover_stations) == 0
-                    or len(crossed_data_set_stations) == 0
-            ):
-                continue
-            stats = data_set.get_profiles_stats(
-                crossed_data_set_stations,
-                crossover_stations,
-                parameter_id,
-                min_depth = min_depth,
-                crossover_radius = crossover_radius,
-                xtype = xtype,
-                only_qc_controlled_data = only_qc_controlled_data,
-            )
-            if (
-                    stats is not None
-                    and 'w_mean' in stats
-                    and stats['w_mean']
-                    and 'w_stdev' in stats
-                    and stats['w_stdev']
-            ):
-                data['w_mean'].append(float(stats['w_mean']))
-                data['w_stdev'].append(float(stats['w_stdev']))
-                data['expocode'].append(stats['expocode'])
-                data['data_set_id'].append(int(stats['data_set_id']))
-                _date = models.DataSet.objects.get(pk=ds[0]).get_timespan(
-                    stations=crossover_stations
-                )[0]
-                data['date'].append(_date.strftime("%Y-%m-%dT00:00:00.0Z"))
+            
+            data = {
+                'w_mean': [],
+                'w_stdev': [],
+                'expocode': [],
+                'data_set_id': [],
+                'date': [],
+            }
+            for ds in data_sets:
+                # Get stations from this data set that falls within the crossover
+                # radius of the original data set
+                crossover_stations = data_set.get_crossover_stations(
+                    stations = data_set_stations,
+                    parameter_id = parameter_id,
+                    crossover_data_set_id = ds[0],
+                    min_depth = min_depth,
+                    minimum_num_stations = minimum_num_stations,
+                    crossover_radius = crossover_radius,
+                )
+                # Get stations from the original data set that match this data set
+                crossed_data_set_stations = data_set.get_crossover_stations(
+                    stations = crossover_stations,
+                    parameter_id = parameter_id,
+                    crossover_data_set_id = data_set_id,
+                    min_depth = min_depth,
+                    crossover_radius = crossover_radius,
+                )
+                if (
+                        len(crossover_stations) == 0
+                        or len(crossed_data_set_stations) == 0
+                ):
+                    continue
+                stats = data_set.get_profiles_stats(
+                    crossed_data_set_stations,
+                    crossover_stations,
+                    parameter_id,
+                    min_depth = min_depth,
+                    crossover_radius = crossover_radius,
+                    xtype = xtype,
+                    only_qc_controlled_data = only_qc_controlled_data,
+                )
+                print('stats \n')
+                print(stats)
+                if (
+                        stats is not None
+                        and 'w_mean' in stats
+                        and stats['w_mean']
+                        and 'w_stdev' in stats
+                        and stats['w_stdev']
+                ):
+                    data['w_mean'].append(float(stats['w_mean']))
+                    data['w_stdev'].append(float(stats['w_stdev']))
+                    data['expocode'].append(stats['expocode'])
+                    data['data_set_id'].append(int(stats['data_set_id']))
+                    _date = models.DataSet.objects.get(pk=ds[0]).get_timespan(
+                        stations=crossover_stations
+                    )[0]
+                    data['date'].append(_date.strftime("%Y-%m-%dT00:00:00.0Z"))
 
-        mean = sum([ 0 if not s else m/pow(s, 2) for m, s in zip(data['w_mean'], data['w_stdev'])])
-        mean = mean / sum([ 0 if not s else 1/pow(s, 2) for s in data['w_stdev']])
-        stdev = sum([ 0 if not s else pow(s, -2) for s in data['w_stdev']])
-        stdev = pow(1/stdev, 1/2)
+            mean = sum([ 0 if not s else m/pow(s, 2) for m, s in zip(data['w_mean'], data['w_stdev'])])
+            mean = mean / sum([ 0 if not s else 1/pow(s, 2) for s in data['w_stdev']])
+            stdev = sum([ 0 if not s else pow(s, -2) for s in data['w_stdev']])
+            stdev = pow(1/stdev, 1/2)
 
-        # Sort by date
-        zipped = sorted(zip(
-            data['date'],
-            data['data_set_id'],
-            data['expocode'],
-            data['w_stdev'],
-            data['w_mean']
-        ))
-        # and unzip
-        (
-            data['date'],
-            data['data_set_id'],
-            data['expocode'],
-            data['w_stdev'],
-            data['w_mean']
-        ) = zip(*zipped)
-        data['stdev'] = [stdev] * len(data['date'])
-        data['mean'] = [mean] * len(data['date'])
-        _date = models.DataSet.objects.get(pk=data_set_id).get_timespan(
-            stations=data_set_stations
-        )[0]
-        data['eval_dataset_date'] = _date.strftime("%Y-%m-%dT00:00:00.0Z")
+            # Sort by date
+            zipped = sorted(zip(
+                data['date'],
+                data['data_set_id'],
+                data['expocode'],
+                data['w_stdev'],
+                data['w_mean']
+            ))
+            # and unzip
+            (
+                data['date'],
+                data['data_set_id'],
+                data['expocode'],
+                data['w_stdev'],
+                data['w_mean']
+            ) = zip(*zipped)
 
-        data = json.dumps(data)
-        cache.set(cache_key, data)
+            data['stdev'] = [stdev] * len(data['date'])
+            data['mean'] = [mean] * len(data['date'])
+            _date = models.DataSet.objects.get(pk=data_set_id).get_timespan(
+                stations=data_set_stations
+            )[0]
+            data['eval_dataset_date'] = _date.strftime("%Y-%m-%dT00:00:00.0Z")
+
+            data = json.dumps(data)
+            print(data)
+            cache.set(cache_key, data)
+
+        except Exception as e:
+            cache.set(cache_key,f'calculation failed:, {e}')
+
+
         if data_return:
             return data
